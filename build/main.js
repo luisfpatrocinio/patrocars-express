b536f8630433792f4bcd3bfa3b8672c979c4f0cc
Luis F Patrocinio <patrocinioluisf@gmail.com>
2024-10-18 10:58:36 -0300

    feat: Ajustes para deploy.

--- build/main.js
+++ build/main.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app_1.default.listen(PORT, () => {
     console.clear();
     console.log(`Listening on PORT: ${PORT}`);
